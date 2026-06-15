64207cc480078b650ee59105698e6b4d54a000d8
ARRahimi <amir9Rah@gmail.com>
2026-06-15 13:47:32 -0500

    Create Event model with schema and fields

---
+++ backend/src/Event.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+/*
+For Event Model:
+  - Campus events will be stored by the event organizers
+  - Admin approval is required before publishing
+*/
+
+const EventSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    date: Date,
+    location: String,
+
+    // provided Google Map link for navigation
+    googleMapsLink: String,
+
+    tags: [String],
+
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending
+    },
+
+    organizer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  { timestamps: true }
+  );
+
+export default mongoose.model("Event", EventSchema);
