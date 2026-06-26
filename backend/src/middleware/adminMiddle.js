90c1aa59bbac61d0734aaaabfad8c0e22982d42f
ARRahimi <amir9Rah@gmail.com>
2026-06-26 13:16:56 -0500

    Update adminMiddle.js

--- backend/src/middleware/adminMiddle.js
+++ backend/src/middleware/adminMiddle.js
@@ -3,7 +3,7 @@ const adminMiddleWare = (req, res, next) => {
     if (req.user.role !== "admin") {
 
         return res.status(403).json({
-            message: "Admin access is required"
+            message: "Admin access is required" 
         });
     }
 
