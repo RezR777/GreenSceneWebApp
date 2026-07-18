c44ddcccc1fdfd3724bae2ed3e9105d4cb60c0ce
ARRahimi <amir9Rah@gmail.com>
2026-07-18 16:46:31 -0500

    Initialize event synchronization job on server start
    Added a call to syncEventsJob to start event synchronization.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import app from "./app.js";
+import syncEventsJob from "./jobs/syncEventsJob.js";
 
 
 dotenv.config();
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 // Establish connection to MongoDB
 connectDB();
+
+syncEventsJob(); // Start event synchronization job
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
