2fd5d7ff2de35cf8f841072a2d03fd2fd7b059b4
ARRahimi <amir9Rah@gmail.com>
2026-07-20 07:33:26 -0500

    Add SearchBar component with input and button

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,1 +1,19 @@
+import "./SearchBar.css";
+
+function SearchBar() {
+  return (
+    <div className="search-bar">
+        <input
+            type="text"
+            placeholder="Searching events..."
+          />
+    
+          <button>
+            Search
+          </button>
+      </div>
+    );
+}
+
+export default SearchBar
 
