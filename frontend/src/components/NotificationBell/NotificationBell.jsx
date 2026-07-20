c15d50116f078bbd0c7fe2961d8fc74c55f8ca7c
ARRahimi <amir9Rah@gmail.com>
2026-07-20 07:30:20 -0500

    Add NotificationBell component with styling

--- frontend/src/components/NotificationBell/NotificationBell.jsx
+++ frontend/src/components/NotificationBell/NotificationBell.jsx
@@ -1,1 +1,12 @@
+import "./NotificationBell.css";
+
+function NotificationBell() {
+  return (
+    <button className="notification-bell">
+        🔔
+    </button>
+  );
+}
+
+export default NotificationBell;
 
