4378e8a9f14246384fddab529bd1d68a19ed4982
ARRahimi <amir9Rah@gmail.com>
2026-07-20 19:53:40 -0500

    Enhance Profile component with user information
    Updated the Profile component to include user details and a profile picture.

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,5 +1,30 @@
-function Profile() {
-  return <h1>Profile</h1>;
-}
+export default function Profile() {
 
-export default Profile;
+return (
+
+    <div className="page">
+    
+        <h1>My Profile</h1>
+
+        <img
+          src="https://placehold.co/150"
+          alt="profile"
+        />
+
+        <h2>John Doe</h2>
+
+        <p>Email: johndoe@my.unt.edu</p>
+
+        <p>Major: Information Technology</p>
+
+        <p>Bio:</p>
+
+        <textarea rows="5" cols="50" />
+
+        <br /><br />
+
+        <button>Edit Profile</button>
+
+  </div>
+  );
+}
