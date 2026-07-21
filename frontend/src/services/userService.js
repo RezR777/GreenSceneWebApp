7205109c6238cb90b6d60b57f2c8c4c3cf02f8e7
ARRahimi <amir9Rah@gmail.com>
2026-07-20 20:54:41 -0500

    Add user profile and saved events service functions

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,1 +1,12 @@
+import api from "./api";
+
+export const getProfile = () =>
+    api.get("/users/profile");
+
+export const updateProfile = (data) =>
+    api.put("/users/profile", data);
+
+export const getSavedEvents = () =>
+    api.get("/users/saved-events");
+
 
