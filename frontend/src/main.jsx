872cd801183da09db686b3703b4cba2726dc9957
ARRahimi <amir9Rah@gmail.com>
2026-07-20 20:56:22 -0500

    Wrap App component with EventsProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
+import { EventsProvider } from "./context/EventContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
+      <EventsProvider>
       <App />
+      </EventsProvider>
     </AuthProvider>
   </StrictMode>
 );
